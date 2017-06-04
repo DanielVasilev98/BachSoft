4d580cb69b1f72952385976f8557825f26642009
ivan4ov4 <i8nov@outlook.com>
2017-06-04 13:38:59 +0300

    Initializing and saving our data

--- BachSoft/StudentsRepository.cs
+++ BachSoft/StudentsRepository.cs
@@ -12,6 +12,29 @@
        {
            string input = Console.ReadLine();
 
+           while (!string.IsNullOrEmpty(input))
+           {
+               string[] tokens = input.Split(' ');
+               string cource = tokens[0];
+               string students = tokens[1];
+                int mark = int.Parse(tokens[2]);
+
+               if (!studentsByCource.ContainsKey(cource))
+               {
+                   studentsByCource.Add(cource, new Dictionary<string, List<int>>());
+               }
+
+               if (!studentsByCource[cource].ContainsKey(students))
+               {
+                   studentsByCource[cource].Add(students,new List<int>());
+               }
+
+               studentsByCource[cource][students].Add(mark);
+               input = Console.ReadLine();
+           }
+
+           isDataInitialized = true;
+            OutputWriter.WriteMessageOnNewLine("Data read!");
        }
 
        public static void InitializeData()
