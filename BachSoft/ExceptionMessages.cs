954f7cd0c7c8ab8ba574b368da6cee7dc498ec3b
ivan4ov4 <i8nov@outlook.com>
2017-06-04 13:32:47 +0300

    update project

---
+++ BachSoft/ExceptionMessages.cs
@@ -0,0 +1,10 @@
+﻿namespace BachSoft
+{
+
+    public static class ExceptionMessages
+    {
+        public const string ExampleErrorMessage = "ErrorMessage!";
+
+        public static string DataAlreadyInitialisedException { get; set; }
+    }
+}
---
+++ BachSoft/StudentsRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace BachSoft
+{
+    using System;
+    using System.Collections.Generic;
+
+   public static class StudentsRepository
+   {
+       public static bool isDataInitialized = false;
+       private static Dictionary<string, Dictionary<string, List<int>>> studentsByCource;
+
+       private static void ReadData()
+       {
+           string input = Console.ReadLine();
+
+       }
+
+       public static void InitializeData()
+       {
+           if (!isDataInitialized)
+           {
+               OutputWriter.WriteMessageOnNewLine("Reading data...");
+               studentsByCource = new Dictionary<string, Dictionary<string, List<int>>>();
+               ReadData();
+           }
+           else
+           {
+               OutputWriter.WriteMessageOnNewLine(ExceptionMessages.DataAlreadyInitialisedException);
+           }
+       }
+
+   }
+}
